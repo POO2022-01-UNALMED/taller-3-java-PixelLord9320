9f386dc09e3a35b60d294de4a3f075eea8a0cdfa
Jhiguitam <jhiguitam@unal.edu.co>
2022-03-27 18:47:25 -0500

    Ahora si XD

---
+++ src/main/java/Marca.java
@@ -0,0 +1,18 @@
+package taller3.televisores;
+
+public class Marca {
+	private String nombre;
+	
+	public Marca(String nombre) { // Constructor marca
+		this.nombre = nombre;
+	}
+	
+	public String getNombre () { // metodo GET
+		return nombre;
+	}
+	
+	public void setNombre (String nombre) { // metodo SET
+		this.nombre = nombre;
+	}
+	
+}
