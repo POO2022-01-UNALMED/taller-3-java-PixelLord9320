4f2f22b61fc5931aebafde2003007edae108640e
Jhiguitam <jhiguitam@unal.edu.co>
2022-03-27 21:19:04 -0500

    iiiiiiiiiiiiiiiiiiiiii

---
+++ src/main/televisores/pruebas.java
@@ -0,0 +1,36 @@
+package taller3.televisores;
+
+public class pruebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		Marca marca1 = new Marca("Semsung");
+	    Marca marca2 = new Marca("Lj");
+		
+	    TV tv1 = new TV(marca1, true);
+	    TV tv2 = new TV(marca2, false);
+	    System.out.println(tv1.getMarca());
+	    
+	    tv1.setPrecio(2000);
+	    tv2.setCanal(90);
+	    tv1.setCanal(121);
+		tv2.setVolumen(7);
+		
+		
+		Control control1 = new Control();
+		control1.enlazar(tv1);
+		control1.turnOff();
+		control1.setCanal(50);
+		control1.turnOn();
+		control1.canalUp();
+		control1.volumenUp();
+		
+	    System.out.println(tv2.getCanal());
+	    System.out.println(tv1.getPrecio());
+	    System.out.println(tv1.getMarca().getNombre());
+	    System.out.println(tv1.getCanal());
+		
+	}
+
+}
